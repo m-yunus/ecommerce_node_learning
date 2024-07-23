fcd65d382938536dacd37d3d03e16095414ff1b9
Yunus <sanuyoonus@gmail.com>
2024-07-23 19:22:04 +0530

    modified

--- routers/category.js
+++ routers/category.js
@@ -31,6 +31,39 @@ router.post('/', async (req, res) => {
     res.status(500).json({ success: false, msg: err.message });
   }
 });
+// Get a single category
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json(category);
+    } catch (err) {
+      res.status(500).json({ success: false, msg: err.message });
+      }
+      });
+      
+//update a category
+router.put('/:id', async (req, res) => {
+  try {
+    // Find the category by ID and update only the name field
+    const updatedCategory = await Category.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true } // This option returns the updated document
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ msg: "Category not found" });
+    }
+
+    res.status(200).json({
+      msg: "Category name updated successfully",
+      data: updatedCategory,
+      status: 200
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: err.message });
+  }
+});
 
 // Delete a category
 router.delete('/:id', async (req, res) => {
